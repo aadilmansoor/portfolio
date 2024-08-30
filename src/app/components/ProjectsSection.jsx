84e125337f847c3f1f17cb0697951de94d9d770b
Aadil Mansoor <aadilmansoor.am@gmail.com>
2024-08-30 15:49:34 +0530

    removed cv

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -59,7 +59,7 @@ const ProjectsSection = () => {
 
       <ul
         ref={ref}
-        className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-12"
+        className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-12 text-center"
       >
         {projectsData.map((project, index) => (
           <motion.li
