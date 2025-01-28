75fc46d72fa46cc78e61ec209484085821f78fce
Aadil Mansoor <aadilmansoor.am@gmail.com>
2025-01-28 23:22:48 +0530

    add projects

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -6,12 +6,12 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
-    title: "Promptopia",
+    title: "Project Management App",
     description:
-      "Developed Promptopia, a Next.js 13 web app for AI prompt discovery and sharing. Features include server-side rendering, file-based routing, and Vercel deployment. Integrated user authentication for secure interactions.",
-    image: "/images/projects/promptopia.png",
-    gitUrl: "https://github.com/aadilmansoor/promptopia",
-    previewUrl: "https://promptopia-nu-tawny.vercel.app/",
+      "Developed a feature-rich project management app using Next.js, Redux, and React-DnD. Enabled task organization into categories (To Do, In Progress, Completed) with drag-and-drop functionality. Integrated AWS services like Cognito for authentication, RDS (PostgreSQL) for database management, Amplify for deployment, and EC2 for scalable hosting. Implemented dynamic project analytics with MUI charts for tracking progress, including bar and pie charts, and ensured a clean, modern user interface with Tailwind CSS.",
+    image: "/images/projects/project-management.png",
+    gitUrl: "https://github.com/aadilmansoor/project-management-app",
+    previewUrl: "https://main.d1gjgygi52kb2o.amplifyapp.com/",
   },
   {
     id: 2,
@@ -24,6 +24,15 @@ const projectsData = [
   },
   {
     id: 3,
+    title: "Promptopia",
+    description:
+      "Developed Promptopia, a Next.js 13 web app for AI prompt discovery and sharing. Features include server-side rendering, file-based routing, and Vercel deployment. Integrated user authentication for secure interactions.",
+    image: "/images/projects/promptopia.png",
+    gitUrl: "https://github.com/aadilmansoor/promptopia",
+    previewUrl: "https://promptopia-nu-tawny.vercel.app/",
+  },
+  {
+    id: 4,
     title: "AI Article Summarizer",
     description:
       "Developed an AI article summarizer using React, Redux Toolkit, and the OpenAI GPT model. Features include URL input, dynamic summary generation, and clipboard functionality for streamlined user experience.",
@@ -32,7 +41,16 @@ const projectsData = [
     previewUrl: "https://astounding-melba-0e2cb4.netlify.app/",
   },
   {
-    id: 4,
+    id: 5,
+    title: "Cinematech",
+    description:
+      "Designed and developed a movie discovery platform using React, featuring a homepage showcasing trending movies and a search functionality for specific titles. Leveraged Rapid API for fetching real-time movie data, ensuring accurate and up-to-date information. The application is optimized for responsive design and user-friendly interaction.",
+    image: "/images/projects/cinematech.png",
+    gitUrl: "https://github.com/aadilmansoor/movie-collections",
+    previewUrl: "https://movie-collections-pied.vercel.app/",
+  },
+  {
+    id: 6,
     title: "Taste Trail",
     description:
       "Created a React project featuring a curated list of New York City restaurants. Utilized JSON data for essential details like reviews, operating hours, and addresses. Users can easily explore dining options with streamlined access to key information. Demonstrates proficiency in React and data integration for user-centric applications.",
